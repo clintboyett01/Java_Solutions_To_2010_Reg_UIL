681dfe28a45d6acfc583687873e131e7539e06b1
clintboyett01 <clin.boyett01@gmail.com>
2018-07-01 03:24:00 -0500

    input and output, as well as 2nd problem

---
+++ Encryption/src/encryption/Encryption.java
@@ -0,0 +1,51 @@
+
+package encryption;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Clint
+ */
+public class Encryption {
+
+    
+    public static void main(String[] args) throws IOException {
+       
+        
+        Scanner sc = new Scanner (new File("encryption.dat"));
+        
+        int count = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < count; i++) {
+            String tempNum = sc.nextLine();
+            String tempNumArry[] = tempNum.split("");
+            //add 7 and take mod
+            for (int j = 0; j < 16; j++) {
+                int temp = Integer.parseInt(tempNumArry[j]);
+                temp += 7;
+                temp %= 10;
+                tempNumArry[j] = "" + temp; 
+            }
+            //switch places
+            for (int j = 0; j < 16; j+=4) {
+                String a = tempNumArry[j];
+                String b = tempNumArry[j+1];
+                
+                tempNumArry[j] = tempNumArry[j+2];
+                tempNumArry[j+2] = a;
+                
+                tempNumArry[j+1] =  tempNumArry[j+3];
+                tempNumArry[j+3] = b;
+            }
+            //print
+            for(String s : tempNumArry){
+                System.out.print(s);
+            }
+            System.out.println("");
+        }
+    }
+    
+}
